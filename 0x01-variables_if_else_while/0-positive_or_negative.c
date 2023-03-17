bda141b2398f6b4ca5fffab25db69ae72dbd782e
Assem1551 <assemmohamed6600@gmail.com>
2023-03-17 20:09:37 +0200

    asas

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/*
-main - prints n is positive or negative
-Return: Returns0
+/**
+* main - prints hello world
+ * Return: Returns 0
 */
 int main(void)
 {
@@ -14,16 +13,16 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 if (n > 0)
-    {
-    printf("%d is positive\n",n);
-    }
+{
+printf("%d is positive\n", n);
+}
 else if (n < 0)
-    {
-    printf("%d is negative\n",n);
-    }
+{
+printf("%d is negative\n", n);
+}
 else
-    {
-    printf("%d is zero\n",n);
-    }
+{
+printf("%d is zero", n);
+}
 return (0);
 }
