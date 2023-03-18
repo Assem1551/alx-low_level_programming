643f974f7242dd362d7b18e09dfd5b35cd368b1f
Assem1551 <assemmohamed6600@gmail.com>
2023-03-18 03:20:59 +0200

    gk

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints alpabet
+ * Return: Returns 0
+*/
+int main(void)
+{
+int ch;
+for (ch = 122 ; ch >= 97 ; ch--)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
