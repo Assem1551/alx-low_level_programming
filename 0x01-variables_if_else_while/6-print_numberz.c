2e27215e695d8377dbd5438e0edaad63f1a13e2a
Assem1551 <assemmohamed6600@gmail.com>
2023-03-18 03:15:49 +0200

    g

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints numbers
+ * Return: Returns 0
+*/
+int main(void)
+{
+int ch;
+for (ch = '0' ; ch <= '9' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+return (0);
+}
