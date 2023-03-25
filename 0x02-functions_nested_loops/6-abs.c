88d4e9c08c56df0c36ba84e5cce6fd5b94a3fc1d
Assem1551 <assemmohamed6600@gmail.com>
2023-03-25 03:09:12 +0200

    Hk

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer.
+ * @x: parameter
+ * Return: return 0
+ */
+int _abs(int x)
+{
+int y = 0;
+
+if (x > 0)
+{
+return (x);
+}
+else if (x == 0)
+{
+return (0);
+}
+else
+{
+y = x / -1;
+return (y);
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of a number.
+ * @x: parameter
+ * Return: returns 0
+ */
+int print_last_digit(int x)
+{
+int y;
+
+y = x % 10;
+if (y < 0)
+{
+y = y * -1;
+}
+_putchar(y + '0');
+return (y);
+}
