b89d76c95a9364fc3f338d76d99afe572e78b467
Assem1551 <assemmohamed6600@gmail.com>
2023-03-25 03:26:35 +0200

    h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+*_isupper - checks for uppercase character
+*@i:parameter
+*Return: 0
+*/
+int _isupper(int i)
+{
+if (i >= 65 && i <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
