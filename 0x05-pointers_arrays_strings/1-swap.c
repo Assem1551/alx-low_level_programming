1608417d8c332fc79d7492ec66dd55eed297aff8
Assem1551 <assemmohamed6600@gmail.com>
2023-03-28 03:48:59 +0200

    f

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *swap_int - swap 2 ints
+ * @a: int parameter
+* @b: 2nd int parameter
+ *RETURN: return nothing
+ */
+void swap_int(int *a, int *b)
+{
+int *temp = *a;
+*a = *b;
+*b = *a;
+
+
+
+
+}
