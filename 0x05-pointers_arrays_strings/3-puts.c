7469fb79586ea6e68cf721d939830b6f5487892f
Assem1551 <assemmohamed6600@gmail.com>
2023-03-28 04:42:58 +0200

    jj

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_puts - swap 2 ints
+ * @str: string
+* Return: Returns the string length
+ */
+
+
+
+void _puts(char *str)
+{
+int i;
+for (i = 0 ; *str = '\0' ; i++)
+{
+_putchar(str[i] + 0);
+}
+_putchar('\n');
+}
