6d858fef9b5586685f9eb8bfd8b9fcb584373539
Assem1551 <assemmohamed6600@gmail.com>
2023-03-31 03:57:48 +0200

    G

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* string_toupper - reverse the arrays
+ * @str: string
+ * Return: Returns str
+*/
+char *string_toupper(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if (str[i] >= 97 && str[i] <= 122)
+{
+str[i] = str[i] - 32;
+}
+}
+return (str);
+}
