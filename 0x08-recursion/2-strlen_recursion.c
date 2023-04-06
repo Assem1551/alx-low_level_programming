245bd6a13edcd03d345be4edc971d93023bcd25f
Assem1551 <assemmohamed6600@gmail.com>
2023-04-06 04:40:44 +0200

    gg

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,10 +6,10 @@
  */
 int _strlen_recursion(char *s)
 {
-        if (*s != '\0')
-        {
-        s++;
-        return (_strlen_recursion(s) + 1);
-        }
-        return (0);
+if (*s != '\0')
+{
+s++;
+return (_strlen_recursion(s) + 1);
+}
+return (0);
 }
