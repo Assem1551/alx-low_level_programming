499823cc784aeb07797eec553778193217197723
Assem1551 <assemmohamed6600@gmail.com>
2023-04-12 03:14:59 +0200

    gg

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,31 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-*main - multiply two numbers 
+*main - multiply two numbers
 *@argc:count parameter
 *@argv:count array
-*@x:sum
 *Return: 0
 */
 
 int main(int argc, char *argv[])
 {
-(void)argc;
 int x;
 
-if (argv[1] == NULL)
+if (argc != 3)
 {
-printf ("Error \n");
+printf("Error\n");
 return (1);
 
 }
 
 x = atoi(argv[1]) * atoi(argv[2]);
-printf ("%d",x);
+printf("%d\n", x);
 return (0);
-
-
-
-
-
 }
