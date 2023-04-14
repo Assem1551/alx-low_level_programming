a4c02964765120b00a819ae3b9bda7529885d94f
Assem1551 <assemmohamed6600@gmail.com>
2023-04-14 09:11:07 +0200

    f

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,20 +1,17 @@
 #include "main.h"
 /**
- * _strspn - Returns the number of bytes
- * @s: parameter1
- * @accept: parameter2
+ * *malloc_checked- Returns the number of bytes
+ * @b: parameter1
  * Return: 0
  */
 
 void *malloc_checked(unsigned int b)
 {
-int *m = malloc (b);
-if ( m==0 )
+int *m = malloc(b);
+if (m == 0)
 {
-exit (98);
+exit(98);
 }
-
-
 return (m);
 
 }
