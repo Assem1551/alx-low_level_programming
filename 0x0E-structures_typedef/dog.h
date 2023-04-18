1bbe1e3c82edb9bf738fc71f28dcccb55406bba9
Assem1551 <assemmohamed6600@gmail.com>
2023-04-18 11:17:16 +0200

    g

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+* struct dog - a dog struct
+* @name: dogs name
+* @age: dogs age
+* @owner: dogs owner
+* Description: just defines anew dog
+*/
 
-struct dog {
+
+struct dog
+{
 char *name;
 float age;
 char *owner;
