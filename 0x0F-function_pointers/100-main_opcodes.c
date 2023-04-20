c23fbdde576f16e7376bbc8d4a16fb9357f84ec7
Assem1551 <assemmohamed6600@gmail.com>
2023-04-20 13:12:30 +0200

    G

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - checksthe code right or wrong
+ * @argc: arg count.
+ * @argv: arg vector.
+ *
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+char *op = (char *) main;
+int i, nbytes;
+
+if (argc != 2)
+{
+printf("Error\n");
+exit(1);
+}
+nbytes = atoi(argv[1]);
+
+if (nbytes < 0)
+{
+printf("Error\n");
+exit(2);
+}
+
+for (i = 0; i < nbytes; i++)
+{
+printf("%02x", op[i] & 0xFF);
+if (i != nbytes - 1)
+printf(" ");
+}
+printf("\n");
+return (0);
+}
