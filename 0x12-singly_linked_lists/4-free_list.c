57a750d40e8be91c87079ffccfddf92d4a160231
Assem1551 <assemmohamed6600@gmail.com>
2023-04-28 11:05:20 +0300

    sum

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -4,17 +4,16 @@
 /**
  * add_node_end - Add a new node at the end of a list.
  * @head: Address of the first node of a list.
- * @str: Address of the string to insert into the new node.
  * Return: Address of the new node.
  **/
 
 void free_list(list_t *head)
 {
- list_t* tmp;
+list_t *tmp;
 while (head)
 {
 tmp = head->next;
-free (head->str);
+free(head->str);
 head = tmp;
 }
 }
