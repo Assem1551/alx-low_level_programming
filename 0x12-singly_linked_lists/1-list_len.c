59a4afa284cbfb707b5278707c46263c8f3bb4b3
Assem1551 <assemmohamed6600@gmail.com>
2023-04-28 09:23:20 +0300

    sum

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ * print_list - Prints singly linked list elements
+ * @h: head of the list
+ * Return: Integer.
+ **/
+
+
+size_t list_len(const list_t *h)
+{
+int sum = 0;
+int i = 0;
+const list_t *tmp;
+tmp = h;
+
+while (h)
+{
+sum ++;
+h = h->next;
+}
+
+return (sum);
